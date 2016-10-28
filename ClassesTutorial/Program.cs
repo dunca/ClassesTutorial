82498758818b47e2571ea3186025587db0688820
flvdnc <flv@mail.com>
2016-10-28 16:53:06 +0300

    ex 1

--- ClassesTutorial/Program.cs
+++ ClassesTutorial/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClassesTutorial
 {
@@ -10,6 +6,8 @@ namespace ClassesTutorial
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This is my first C# project!");
+            Console.ReadKey();
         }
     }
 }
