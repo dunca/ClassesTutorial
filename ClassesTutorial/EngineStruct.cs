03e9ceb6ce373d3564fd543131950756d0d7fcbb
flvdnc <flv@mail.com>
2016-10-28 17:01:03 +0300

    ex 5

---
+++ ClassesTutorial/EngineStruct.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ClassesTutorial
+{
+    struct EngineStruct
+    {
+    }
+}
--- ClassesTutorial/Program.cs
+++ ClassesTutorial/Program.cs
@@ -9,6 +9,8 @@ namespace ClassesTutorial
             Mercedes mercedes = new Mercedes();
             //AbstractCar aCar = new AbstractCar();  // can't instantiate an abstract class
 
+            EngineStruct engine = new EngineStruct();
+
             Console.WriteLine("This is my first C# project!");
             Console.ReadKey();
         }
