ab4fb4efd9eedab6243bae360a57df2cbb799b93
flvdnc <flv@mail.com>
2016-10-28 17:15:49 +0300

    ex 9

--- ClassesTutorial/Mercedes.cs
+++ ClassesTutorial/Mercedes.cs
@@ -2,13 +2,14 @@
 
 namespace ClassesTutorial
 {
-    public class Mercedes : AbstractCar
+    protected class Mercedes : AbstractCar // won't compile
     {
         private string _description;
         private int _price;
 
-        public string Description { get { return _description; } set { _description = value; } }
-        public int Price { get { return _price; } set { _price = value; } }
+        // won't be accessible from within Program.cs
+        protected string Description { get { return _description; } set { _description = value; } }
+        protected int Price { get { return _price; } set { _price = value; } }
 
         public Mercedes()
         {
