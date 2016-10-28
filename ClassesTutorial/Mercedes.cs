135ce6f67b56a15d975cc4db64034b1872751473
flvdnc <flv@mail.com>
2016-10-28 17:05:33 +0300

    ex 6 cont.

--- ClassesTutorial/Mercedes.cs
+++ ClassesTutorial/Mercedes.cs
@@ -4,9 +4,13 @@ namespace ClassesTutorial
 {
     public class Mercedes : AbstractCar
     {
+        public string description;
+        public int price;
+
         public Mercedes()
         {
-
+            description = "E Class";
+            price = 67000;
         }
     }
 }
